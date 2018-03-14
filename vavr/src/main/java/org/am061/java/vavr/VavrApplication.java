0e53cdbc21a7797023027c7835ca552769d4ab42
Wajdzik Maciej <wajdzik.maciej@gmail.com>
2018-03-14 18:11:17 +0100

    Vavr Option + Tuple

---
+++ vavr/src/main/java/org/am061/java/vavr/VavrApplication.java
@@ -0,0 +1,18 @@
+package org.am061.java.vavr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VavrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VavrApplication.class, args);
+    }
+}
+
+/*
+    http://www.baeldung.com/vavr
+
+    Vavr - a functional library for Java 8+ that provides immutable data types and functional control structures.
+ */
