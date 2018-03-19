196201613fa70f39a4a2958e77c4c55731d8c924
Wajdzik Maciej <wajdzik.maciej@gmail.com>
2018-03-19 13:51:31 +0100

    RxJava Observable

--- rxjava/src/main/java/org/am061/java/rxjava/RxJavaApplication.java
+++ rxjava/src/main/java/org/am061/java/rxjava/RxJavaApplication.java
@@ -13,4 +13,51 @@ public class RxJavaApplication {
 
 /*
     http://www.baeldung.com/rxjava-tutorial
+
+    At a glance, the RxJava may look similar to Java 8 Streams, but in fact,
+    it is much more flexible and fluent, making it a powerful programming paradigm.
+
+    On one side, functional programming is the process of building software by composing pure functions,
+    avoiding shared state, mutable data, and side-effects. On the other side, reactive programming is
+    an asynchronous programming paradigm concerned with data streams and the propagation of change.
+
+    Together, functional reactive programming forms a combination of functional and reactive techniques that
+    can represent an elegant approach to event-driven programming – with values that change over time and
+    where the consumer reacts to the data as it comes in.
+
+    Reactive Manifesto:
+        Responsive – systems should respond in a timely manner
+        Message Driven – systems should use async message-passing between components to ensure loose coupling
+        Elastic – systems should stay responsive under high load
+        Resilient – systems should stay responsive when some components fail
+
+    Observable represents any object that can get data from a data source and whose state may be of interest
+    in a way that other objects may register an interest.
+
+    An observer is any object that wishes to be notified when the state of another object changes
+
+    An observer subscribes to an Observable sequence.
+    The sequence sends items to the observer one at a time.
+    The observer handles each one before processing the next one.
+    If many events come in asynchronously, they must be stored in a queue or dropped.
+
+    In Rx, an observer will never be called with an item out of order or
+    called before the callback has returned for the previous item.
+
+    Two types of Observable:
+
+        Non-Blocking – asynchronous execution is supported and is allowed to unsubscribe at any point in the event stream.
+        Blocking – all onNext observer calls will be synchronous, and it is not possible to unsubscribe
+                   in the middle of an event stream. We can always convert an Observable into a Blocking Observable, using the method toBlocking
+
+
+    An operator is a function that takes one Observable (the source) as its first argument
+    and returns another Observable (the destination). Then for every item that the source observable emits,
+    it will apply a function to that item, and then emit the result on the destination Observable.
+
+    Operators can be chained together to create complex data flows that filter event based on certain criteria.
+    Multiple operators can be applied to the same observable.
+
+    It is not difficult to get into a situation in which an Observable is emitting items faster
+    than an operator or observer can consume them - back-pressure problem.
  */
--- rxjava/src/test/java/org/am061/java/rxjava/ObservableTest.java
+++ rxjava/src/test/java/org/am061/java/rxjava/ObservableTest.java
@@ -3,23 +3,63 @@ package org.am061.java.rxjava;
 import io.reactivex.Observable;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ObservableTest {
 
     private String result = "";
 
     @Test
-    public void methods() {
+    public void create() {
+        Observable<String> observable = Observable.just("Hello");
+        observable.subscribe(s -> result = s);
+
+        assertEquals("Hello", result);
+    }
+
+    @Test
+    public void callbacks() {
         String[] letters = {"a", "b", "c", "d", "e", "f", "g"};
 
         Observable<String> observable = Observable.fromArray(letters);
         observable.subscribe(
-                i -> result += i,               // OnNext
-                Throwable::printStackTrace,     // OnError
-                () -> result += "_Completed"    // OnCompleted
+                i -> result += i,               // OnNext - called on our observer each time a new event is published to the attached Observable
+                Throwable::printStackTrace,     // OnError - called when an unhandled exception is thrown during the RxJava framework code or our event handling code
+                () -> result += "_Completed"    // OnCompleted - called when the sequence of events associated with an Observable is complete
         );
 
-        assertTrue(result.equals("abcdefg_Completed"));
+        assertEquals("abcdefg_Completed", result);
+    }
+
+    @Test
+    public void transformationsMap() {
+        String[] letters = {"a", "b", "c", "d", "e", "f", "g"};
+
+        Observable.fromArray(letters)
+                .map(String::toUpperCase)
+                .subscribe(letter -> result += letter);
+
+        assertEquals("ABCDEFG", result);
+    }
+
+    @Test
+    public void transformationsFlatMap() {
+        Observable.just("book1", "book2")
+                .flatMap(s -> Observable.fromArray(s.split("")))
+                .subscribe(l -> result += l);
+
+        assertEquals("book1book2", result);
+    }
+
+    // scan() allows us to carry forward state from event to event
+    @Test
+    public void transformationsScan() {
+        String[] letters = {"a", "b", "c"};
+
+        Observable.fromArray(letters)
+                .scan(new StringBuilder(), StringBuilder::append)
+                .subscribe(total -> result += total.toString());
+
+        assertEquals("aababc", result);
     }
 }
